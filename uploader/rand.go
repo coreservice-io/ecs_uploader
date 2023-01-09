fc025fa9b30f578454ee47c7dc77b9d93886d07d
leo <leo@leos-MacBook-Pro.local>
2023-01-09 17:31:58 +0800

    update

--- uploader/rand.go
+++ uploader/rand.go
@@ -3,42 +3,22 @@ package uploader
 import (
 	"math/rand"
 	"time"
-	"unsafe"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyz"
-
-const letterBytes_len = len(letterBytes)
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-var randsrc = rand.NewSource(time.Now().UnixNano())
-
-func genRandStr(n int64) string {
-	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, randsrc.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = randsrc.Int63(), letterIdxMax
-		}
-		if idx := cache & letterIdxMask; idx < int64(letterBytes_len) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
+func genRandStr(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
-	return *(*string)(unsafe.Pointer(&b))
+	return string(b)
 }
 
 func (upl *Uploader) GenRandIdStr() string {
-	x := genRandStr(42)
-	return x
+	return genRandStr(42)
 }
